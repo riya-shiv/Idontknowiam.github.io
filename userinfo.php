1f58f11e2cc1a8cb11dde52ea4e8fe3180d3071d
shiv rawat <65090022+riya-shiv@users.noreply.github.com>
2020-05-10 21:52:22 -0700

    Add files via upload
    osm

---
+++ userinfo.php
@@ -0,0 +1,16 @@
+<?php
+$con = mysqli_connect('localhost','root');
+if($con){
+    echo "conection successful";
+}else{
+    echo "on connection";
+}
+mysqli_select_db($con,'youtubeuserdata');
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "insert into userinfodata (username, password) 
+Values ('$username', '$password')";
+echo "$query";
+mysqli_query($con, $query);
+?>
